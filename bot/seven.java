dfb360512d09a679ddb1b0caa154323dd09f0ab8
tarunsai <kakarla.sai3@gmail.com>
2025-09-17 20:21:19 +0530

    updated the  files

---
+++ bot/seven.java
@@ -0,0 +1,5 @@
+public class Seven {
+    public static void main(String[] args) {
+        System.out.println("Hello from Seven.java!");
+    }
+}
--- bot/six.java
+++ bot/six.java
@@ -1,1 +1,2 @@
 this is for the crm test how it will work
+System.out.println("CRM test is running successfully.");
